fe863355d085b4f1df1f9b559db5854682564417
Billy Hare <bchazhare@gmail.com>
2019-12-15 22:51:45 -0500

    Replace hard-coded receive phone number with environment variable.

--- send_sms.py
+++ send_sms.py
@@ -5,10 +5,11 @@ from twilio.rest import Client
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 my_twilio_phone_number = os.environ["TWILIO_PHONE_NUMBER"]
+receive_phone_number = os.environ["MY_PHONE_NUMBER"]
 test_text = "This is a test.  This is only a test."
 
 client = Client(account_sid, auth_token)
 
-client.messages.create(to="+17706341811",
+client.messages.create(to=receive_phone_number,
                        from_=my_twilio_phone_number,
                        body=test_text)
