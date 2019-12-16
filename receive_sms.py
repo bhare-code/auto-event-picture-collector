b6291d4195a122cb8dff4c55ecec80124fa11c89
Billy Hare <bchazhare@gmail.com>
2019-12-15 22:16:26 -0500

    Committing working send_sms.py and receive_sms.py scripts.

---
+++ receive_sms.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+resp_text = "The Robots are coming! Head for the hills!"
+app = Flask(__name__)
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    resp = MessagingResponse()
+    resp.message(resp_text)
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ wedding_server.py
@@ -0,0 +1,31 @@
+import requests
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+DOWNLOAD_DIRECTORY = '/home/pi/Pictures'
+app = Flask(__name__)
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming with a simple text message."""
+
+    resp = MessagingResponse()
+
+    if request.values['NumMedia'] != '0':
+
+        # Use the message SID as a filename.
+        filename = request.values['MessageSid']   '.png'
+        with open('{}/{}'.format(DOWNLOAD_DIRECTORY, filename), 'wb') as f:
+           image_url = request.values['MediaUrl0']
+           f.write(requests.get(image_url).content)
+
+        resp.message("Thanks for the image!")
+    else:
+        resp.message("Try sending a picture message.")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
